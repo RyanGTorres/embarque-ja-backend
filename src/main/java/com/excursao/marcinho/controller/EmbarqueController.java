5d379eab49b37d7123abec48250a86aaca34ff9e
RyanGTorres <ryryan009@gmail.com>
2025-08-11 19:40:08 -0300

    feat: adiciona tratamento documentação do projeto usando SWAGGER

--- src/main/java/com/excursao/marcinho/controller/EmbarqueController.java
+++ src/main/java/com/excursao/marcinho/controller/EmbarqueController.java
@@ -1,8 +1,14 @@
 package com.excursao.marcinho.controller;
 
 import com.excursao.marcinho.dto.request.EmbarqueRequest;
+import com.excursao.marcinho.dto.response.ClienteResponse;
 import com.excursao.marcinho.dto.response.EmbarqueResponse;
 import com.excursao.marcinho.service.EmbarqueService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
@@ -16,30 +22,45 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/embarque")
 @RequiredArgsConstructor
+@Tag(name = "Embarque", description = "Recurso responsavel pelo gerencimaneto de embarque")
 public class EmbarqueController {
     private final EmbarqueService service;
 
+    @Operation(summary = "Salvar embarque", description = "Metodo responsavel pelo salvamento de embarque")
+    @ApiResponse(responseCode = "201", description = "Embarque salvo com sucesso!",content = @Content(schema = @Schema(implementation = EmbarqueResponse.class)))
+    @PostMapping
+    public ResponseEntity<EmbarqueResponse> save (@Valid @RequestBody EmbarqueRequest embarqueRequest){
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.save(embarqueRequest));
+    }
+
+    @Operation(summary = "Buscar embarque pelo ID", description = "Metodo responsavel pela busca de embarque pelo seu ID")
+    @ApiResponse(responseCode = "200", description = "Embarque encontrado com sucesso!",content = @Content(schema = @Schema(implementation = EmbarqueResponse.class)))
+    @ApiResponse(responseCode = "404", description = "Embarque não foi encontrado!", content = @Content())
     @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     public EmbarqueResponse findById(@PathVariable Long id){
         return service.findByID(id);
     }
 
+    @Operation(summary = "Buscar todos os embarques", description = "Metodo responsavel pela busca de todos os embarques")
+    @ApiResponse(responseCode = "200", description = "Embarques encontrado com sucesso!",content = @Content(schema = @Schema(implementation = EmbarqueResponse.class)))
+    @ApiResponse(responseCode = "404", description = "Embarques não foi encontrado!", content = @Content())
     @GetMapping
     public ResponseEntity<List<EmbarqueResponse>> findAll(){
         return ResponseEntity.ok(service.findAll());
     }
 
-    @PostMapping
-    public ResponseEntity<EmbarqueResponse> save (@Valid @RequestBody EmbarqueRequest embarqueRequest){
-        return ResponseEntity.status(HttpStatus.CREATED).body(service.save(embarqueRequest));
-    }
-
+    @Operation(summary = "Atualizar embarque pelo ID", description = "Metodo responsavel pela atualização do embarque pelo seu ID")
+    @ApiResponse(responseCode = "200", description = "Embarque atualizado com sucesso!",content = @Content(schema = @Schema(implementation = EmbarqueResponse.class)))
+    @ApiResponse(responseCode = "404", description = "Embarque não foi encontrado!", content = @Content())
     @PutMapping("/{id}")
     public ResponseEntity<EmbarqueResponse> update (@PathVariable Long id, @Valid @RequestBody EmbarqueRequest embarqueRequest){
         return ResponseEntity.ok(service.update(id, embarqueRequest));
     }
 
+    @Operation(summary = "Deletar embarque", description = "Método para deletar embarque pelo ID")
+    @ApiResponse(responseCode = "204", description = "Embarque deletado com sucesso")
+    @ApiResponse(responseCode = "404", description = "Embarque não encontrado para exclusão")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete (@PathVariable Long id){
         service.deleteByID(id);
--- src/main/java/com/excursao/marcinho/controller/ExcursaoController.java
+++ src/main/java/com/excursao/marcinho/controller/ExcursaoController.java
@@ -1,8 +1,15 @@
 package com.excursao.marcinho.controller;
 
 import com.excursao.marcinho.dto.request.ExcursaoRequest;
+import com.excursao.marcinho.dto.response.ClienteResponse;
+import com.excursao.marcinho.dto.response.EmbarqueResponse;
 import com.excursao.marcinho.dto.response.ExcursaoResponse;
 import com.excursao.marcinho.service.ExcursaoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -14,29 +21,44 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/excursao")
 @RequiredArgsConstructor
+@Tag(name = "Excursao", description = "Recurso responsavel pelo gerencimaneto de excursao")
 public class ExcursaoController {
     private final ExcursaoService excursaoService;
 
-    @GetMapping("/{id}")
-    public ResponseEntity<ExcursaoResponse> findByID(@PathVariable Long id){
-        return ResponseEntity.ok(excursaoService.findById(id));
-    }
-
-    @GetMapping
-    public ResponseEntity<List<ExcursaoResponse>> findAll(){
-        return ResponseEntity.ok(excursaoService.findAll());
-    }
-
+    @Operation(summary = "Salvar excursao", description = "Metodo responsavel pelo salvamento de excursao")
+    @ApiResponse(responseCode = "201", description = "Embarque salvo com sucesso!",content = @Content(schema = @Schema(implementation = ExcursaoResponse.class)))
     @PostMapping
     public ResponseEntity<ExcursaoResponse> save(@Valid @RequestBody ExcursaoRequest excursaoRequest){
         return ResponseEntity.status(HttpStatus.CREATED).body(excursaoService.save(excursaoRequest));
     }
 
+    @Operation(summary = "Buscar excursao pelo ID", description = "Metodo responsavel pela busca de excursao pelo seu ID")
+    @ApiResponse(responseCode = "200", description = "Embarque encontrado com sucesso!",content = @Content(schema = @Schema(implementation = ExcursaoResponse.class)))
+    @ApiResponse(responseCode = "404", description = "Embarque não foi encontrado!", content = @Content())
+    @GetMapping("/{id}")
+    public ResponseEntity<ExcursaoResponse> findByID(@PathVariable Long id){
+        return ResponseEntity.ok(excursaoService.findById(id));
+    }
+
+    @Operation(summary = "Buscar todos os excursões", description = "Metodo responsavel pela busca de todos os excursões")
+    @ApiResponse(responseCode = "200", description = "Excursão encontrado com sucesso!",content = @Content(schema = @Schema(implementation = ExcursaoResponse.class)))
+    @ApiResponse(responseCode = "404", description = "Excursão não foi encontrado!", content = @Content())
+    @GetMapping
+    public ResponseEntity<List<ExcursaoResponse>> findAll(){
+        return ResponseEntity.ok(excursaoService.findAll());
+    }
+
+    @Operation(summary = "Atualizar excursao pelo ID", description = "Metodo responsavel pela atualização do excursao pelo seu ID")
+    @ApiResponse(responseCode = "200", description = "Excursão atualizado com sucesso!",content = @Content(schema = @Schema(implementation = ExcursaoResponse.class)))
+    @ApiResponse(responseCode = "404", description = "Excursão não foi encontrado!", content = @Content())
     @PutMapping("/{id}")
     public ResponseEntity<ExcursaoResponse> update(@PathVariable Long id, @Valid @RequestBody ExcursaoRequest excursaoRequest){
         return ResponseEntity.ok(excursaoService.update(id, excursaoRequest));
     }
 
+    @Operation(summary = "Deletar excursao", description = "Método para deletar excursão pelo ID")
+    @ApiResponse(responseCode = "204", description = "Excursão deletado com sucesso")
+    @ApiResponse(responseCode = "404", description = "Excursão não encontrado para exclusão")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteById(@PathVariable Long id){
         excursaoService.deleteById(id);
